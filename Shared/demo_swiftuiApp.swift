debe8cdcadee33b41a28a46d596bd78670e0f020
Jorge Saavedra <jsaavedra@montran.com>
2022-05-31 15:59:52 -0500

    Initial Commit

---
+++ Shared/demo_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  demo_swiftuiApp.swift
+//  Shared
+//
+//  Created by Jorge Saavedra on 31/5/22.
+//
+
+import SwiftUI
+
+@main
+struct demo_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
